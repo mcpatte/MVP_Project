ce91730d20cdbfde3f6f34e12f06d67fb16171f7
Chase Patterson <mcpatte@gmail.com>
2016-07-11 23:54:39 -0700

    added random numbers

--- client/js/dieController.js
+++ client/js/dieController.js
@@ -2,4 +2,7 @@ angular.module('gg.DieController', [])
 
 .controller('DieController', function($scope){
   $scope.message = "Hey from controller"
+  $scope.dieRoll = function(){
+    $scope.results = Math.floor(Math.random() * 9)+1;
+  }
 })
