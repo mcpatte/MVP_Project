cb2223ac423cccd86c4318982be2bfdadae29d69
Chase Patterson <mcpatte@gmail.com>
2016-07-11 23:06:48 -0700

    added bower to package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
-module.exports = function(grunt){
+module.exports = function(grunt) {
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
@@ -7,10 +8,101 @@ module.exports = function(grunt){
       },
       dist: {
         src: [
-            'client/**/*.js'
+            'client/**/*.js', '!client/js/bower_components/**/*.*'
         ],
         dest: 'dist/production.js'
       }
     },
-  })
-}
+
+    nodemon: {
+      dev: {
+        script: 'app.js'
+      }
+    },
+
+    uglify: {
+      dist: {
+        files: {
+          'dist/<%=production%>production.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+
+    cssmin: {
+    },
+
+    watch: {
+      scripts: {
+        files: [
+          'client/**/*.js',
+          'lib/**/*.js',
+        ],
+        tasks: [
+          'concat',
+          'uglify'
+        ]
+      },
+      css: {
+        files: 'client/*.css',
+        tasks: ['cssmin']
+      }
+    },
+
+    shell: {
+      prodServer: {
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  grunt.registerTask('server-dev', function (target) {
+    // Running nodejs in a different process and displaying output on the main console
+    var nodemon = grunt.util.spawn({
+         cmd: 'grunt',
+         grunt: true,
+         args: 'nodemon'
+    });
+    nodemon.stdout.pipe(process.stdout);
+    nodemon.stderr.pipe(process.stderr);
+
+    grunt.task.run([ 'watch' ]);
+  });
+
+  grunt.registerTask('heroku:production', 'build');
+
+  ////////////////////////////////////////////////////
+  // Main grunt tasks
+  ////////////////////////////////////////////////////
+
+  grunt.registerTask('test', [
+    'mochaTest', 'jshint'
+  ]);
+
+  grunt.registerTask('build', [
+    'concat', 'uglify'
+  ]);
+
+  grunt.registerTask('default', [
+    'build'
+  ]);
+
+  grunt.registerTask('upload', function(n) {
+    if(grunt.option('prod')) {
+      // add your production server task here
+    } else {
+      grunt.task.run([ 'server-dev' ]);
+    }
+  });
+
+  grunt.registerTask('deploy', [
+    'test', 'build'
+  ]);
+
+
+};
