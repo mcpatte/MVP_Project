103fb41c001244516e2563e058260503e020fa67
Chase Patterson <mcpatte@gmail.com>
2016-07-11 18:13:11 -0700

    moved app.js to root and set express to serve static index

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var app = express();
 app.set('port', (process.env.PORT || 3000))
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use(express.static(__dirname + '/client'))
 
 app.listen(app.get('port'), function () {
-  console.log('Example app listening on port: ' + app.get('port'));
-});
+  console.log('Example app listening on port:' + app.get('port'));
+})
